26cd48f1d5241a91edb8489f4d853dc6673fab92
EMPChief <bjornmagnekristensen1999@gmail.com>
2023-12-18 21:45:47 +0100

    Made a bat script that intelize tailwind

---
+++ setups.bat
@@ -0,0 +1,8 @@
+:: Initialize Tailwind CSS compatible Django app
+python manage.py tailwind init
+
+:: Install django-tailwind
+python -m pip install django-tailwind
+
+:: Install Tailwind CSS dependencies
+python manage.py tailwind install
